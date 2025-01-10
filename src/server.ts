a62ffe9a8c8753930484fe1578d34096ab18b6f9
femke77 <megan.meyers.388@gmail.com>
2025-01-09 18:52:49 -0800

    removed code for serving client on same port via express static

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,10 @@ import { expressMiddleware } from "@apollo/server/express4";
 import { authenticateToken } from "./utils/auth.js";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import db from "./config/connection.js";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// import { fileURLToPath } from "node:url";
+// import path from "node:path";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
@@ -33,14 +33,15 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/dist as static assets so everything works off one port,
   // no more dev server on client.
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-    // necessary for client-side routing to work
-    app.get("*", (_req, res) => {
-      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
-    });
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+  //   // necessary for client-side routing to work
+  //   app.get("*", (_req, res) => {
+  //     res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  //   });
+  // }
 
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
